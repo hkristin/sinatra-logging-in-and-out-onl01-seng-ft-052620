14135c79e378870af08b0bbdb19cdad5ab2280ef
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-16 01:49:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
   @user = User.find_by(:username => params[:username])
-  if 
+  if @user !=
     @user.save
     session[:user_id] = @user.id
   end
